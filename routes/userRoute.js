0b44a4f8d02f200e9e53ee6ea58fef4b822f95de
mohd humaid <139005276+mohdhumaidd19@users.noreply.github.com>
2024-03-30 01:37:53 +0530

    Add files via upload

---
+++ routes/userRoute.js
@@ -0,0 +1,11 @@
+import express from 'express';
+import { Prisma } from '@prisma/client';
+import {  allFav, bookVisit, cancelBooking, createUser, getAllBookings, toFav } from '../controllers/userController.js';
+const router =express.Router();
+router.post("/register",createUser);
+router.post("/bookVisit/:id",bookVisit)
+router.post("/allBookings",getAllBookings)
+router.post("/removeBooking/:id",cancelBooking)
+router.post("/toFav/:rid",toFav);
+router.post("/allFav/",allFav);
+export {router as userRoute}
